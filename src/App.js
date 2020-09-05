880e88eb9ebd0841f062a8f6dad68e75cdd84f72
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-05 20:55:59 +0200

    Facebook clone, let's go

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Facebook clone is coming</h1>
+
+      {/* Header */}
+
+      {/* App body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCWLPtrP4i_hxWydzUI2Y6u_Ov5i5f1tjg",
+  authDomain: "fb-clone-8129f.firebaseapp.com",
+  databaseURL: "https://fb-clone-8129f.firebaseio.com",
+  projectId: "fb-clone-8129f",
+  storageBucket: "fb-clone-8129f.appspot.com",
+  messagingSenderId: "304446927007",
+  appId: "1:304446927007:web:4b83aed4fd52b30a6bf2aa",
+  measurementId: "G-77FLLH0QLF"
+};
