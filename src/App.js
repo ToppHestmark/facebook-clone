a85ed2e148bf66ef0b366a219f2b6ffe28a11309
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-06 10:16:23 +0200

    Styled header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header  from './Header'
 
 function App() {
   return (
     <div className="App">
-      <h1>Facebook clone is coming</h1>
 
-      {/* Header */}
+      <Header />
 
       {/* App body */}
         {/* Sidebar */}
