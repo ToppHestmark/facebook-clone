79d4df9cb2bc0f156f844031ffbf0eb4c2b88059
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-06 20:27:48 +0200

    Added widget on the right side

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header  from './components/Header';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
+import Widgets from './components/Widgets';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <div className="app__body">
         <Sidebar />
         <Feed />
+        <Widgets />
       </div>
         
     </div>
