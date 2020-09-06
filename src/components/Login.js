68d0d707d57bd097688fb6e180d7297cae21ff6b
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-06 22:41:45 +0200

    dispatched user avatar within the authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,14 +2,21 @@ import React from 'react';
 import './Login.css'
 import { Button } from '@material-ui/core';
 import { auth, provider } from './../firebase';
+import { useStateValue } from './StateProvider';
+import { actionTypes } from './reducer';
 
 function Login() {
+  const [state, dispatch] = useStateValue()
 
   const signIn = () => {
     // Sign in stuff
     auth
     .signInWithPopup(provider)
     .then(result => {
+      dispatch({
+        type: actionTypes.SET_USER,
+        user: result.user,
+      })
       console.log(result.user)
     })
     .catch((error) => alert(error.message))
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,11 +8,16 @@ import ChatIcon from '@material-ui/icons/Chat';
 import StorefrontIcon from '@material-ui/icons/Storefront';
 import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
 import { ExpandMoreOutlined } from '@material-ui/icons';
+import { useStateValue } from './StateProvider';
 
 function Sidebar() {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="sidebar">
-      <SidebarRow src="https://avatars3.githubusercontent.com/u/48948074?s=460&u=d8d5a56c477f72be28e39cd0f40b51243180458a&v=4" title="Topp Hestmark" 
+      <SidebarRow 
+      src={user.photoURL} 
+      title={user.displayName}
       />
       <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Information Center" />
       <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
